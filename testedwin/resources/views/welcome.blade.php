babc625336f78b308402dad6cb36bf6bda53badf
Fred A. Stewart <fredastewart@gmail.com>
2016-08-02 19:30:40 -0400

    On branch master

--- testedwin/resources/views/welcome.blade.php
+++ testedwin/resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5.2 for Edwin</div>
+                <div class="title">Laravel 5.2 for Edwin Diaz</div>
             </div>
         </div>
     </body>
